d2cc5ee4a666ccecf791a9a96a48c369021ba1eb
Xaitam <mattiashn8@hotmail.com>
2019-10-16 10:32:31 +0200

    Fixed stupid mistake

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const server = express();
 const port = 4000;
 
 server.get("/", (req, res) => {
-   res.sendFile(__dirname + '/index.html');
+   res.sendFile(__dirname + '/public/index.html');
 });
 
 server.get("/json", (req, res) => {
