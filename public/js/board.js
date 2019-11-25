098143394be9789d60dd481b0c12823d274df409
eyvind yang <eyviny17@uia.no>
2019-11-25 12:03:24 +0100

    clean code

--- public/js/board.js
+++ public/js/board.js
@@ -1,25 +1,15 @@
-//---------------------------------------------------------------------------- Global Variables
-//Base url 
+
 let url = "http://localhost:3000";
 
-//div lists
 let div_list;
-
-//form - add list
 let form_show_btn_add_list;
 let form_add_list;
 let name_add_list = document.getElementById('name_add_list');
-
-//add list values
 let addlist_name;
 let addlist_txtResult;
-
-//new task form
 let taskForm;
 let cancelButton;
 let formButton = document.getElementById("makeTaskID");
-
-// Delete User Form
 let deleteUserForm;
 let deleteUser;
 let confirmDelete;
@@ -27,53 +17,33 @@ let cancelDelete;
 
 window.addEventListener("load", setupBoard);
 
-//---------------------------------------------------------------------------- Functions
 function setupBoard() {
-    //---------------------------------------------------------------------------- Variables
     div_list = document.getElementById("lists");
-
-    //Add List Form 
     form_show_btn_add_list = document.getElementById("form_show_btn_add_list");
     form_add_list = document.getElementById("form_add_list");
     form_add_btn_add_list = document.getElementById("form_add_btn_add_list");
     form_close_btn_add_list = document.getElementById("form_close_btn_add_list");
     addlist_name = this.document.getElementById("name_add_list");
     addlist_txtResult = this.document.getElementById("txtResult_add_list");
-
-
     formButton = document.getElementById("makeTaskID");
-
-    // Delete User Form
     deleteUserForm = document.getElementById("deleteUserForm");
     deleteUser = document.getElementById("deleteUser");
     confirmDelete = document.getElementById("confirmDelete");
     cancelDelete = document.getElementById("cancelDelete");
 
-    //---------------------------------------------------------------------------- Eventlistners
-    //Delete User Button
     deleteUser.addEventListener("click", deleteUserWindow);
     cancelDelete.addEventListener("click", cancelResponse);
-
-    //Show add list form 
     form_show_btn_add_list.addEventListener('click', show_form_add_list);
-    form_close_btn_add_list.addEventListener('click', show_form_add_list);
-
-    //Add add_list function to add_list button 
+    form_close_btn_add_list.addEventListener('click', show_form_add_list); 
     form_add_btn_add_list.addEventListener('click', add_list);
 
-    // Execute a function when the user releases a key on the keyboard
     name_add_list.addEventListener("keyup", function (event) {
-        // Number 13 is the "Enter" key on the keyboard
         if (event.keyCode === 13) {
-            // Cancel the default action, if needed
             event.preventDefault();
-            // Trigger the button element with a click
             document.getElementById("form_add_btn_add_list").click();
         }
         if (event.keyCode === 27) {
-            // Cancel the default action, if needed
             event.preventDefault();
-            // Trigger the button element with a click
             document.getElementById("form_close_btn_add_list").click();
         }
     });
@@ -82,18 +52,11 @@ function setupBoard() {
 
 }
 
-
-
 async function get_list() {
-
-    //Clear the field for all list 
     div_list.innerHTML = "";
 
-    //get the login data
     let logindata = JSON.parse(sessionStorage.getItem("user"));
     let token = logindata.token;
-
-    //Get list url 
     let urlList = url + "/list";
     let urlTask = url + "/list/task";
 
@@ -111,10 +74,6 @@ async function get_list() {
         if (respList.status > 202) {
             throw dataList;
         }
-
-        //console.log("Getting lists");
-        //div_list.innerHTML = "";
-
         let idInc = 1
         for (let valueList of dataList) {
             
@@ -127,19 +86,14 @@ async function get_list() {
             let cancelButtonID = "cancelButton-" + idInc;
             let addTaskID = "addTask-" + idInc;
             let taskInputID = "taskInput-" + idInc;
-
             let idTaskInc = 1;
 
-            //Tasks names setup: [nameOnTheThing]-[List ID]-[TASK INCREMENTED NUMBER]
             let taskButtonID = "taskButton-" + valueList.id + "-";
             let newTaskID = "newTask-" + valueList.id + "-";
             let taskMenuID = "taskMenu-" + valueList.id + "-";
             let taskNameID = "taskName-" + valueList.id + "-";
             let deleteTaskID = "deleteTask-" + valueList.id + "-";
-
-            //Used to add eventlistners
             let taskIdArray = [];
-
             let publicText = "";
             let textLabelMakePublic = "Make Public";
             let textLabelMakePrivate = "Make Private";
@@ -150,7 +104,6 @@ async function get_list() {
             else {
                 publicText = textLabelMakePublic;
             }
-
 
             let html = `
                 <input id="${editID}"class="list-title" value="${valueList.name}"></input>
@@ -175,9 +128,7 @@ async function get_list() {
             let htmlTask = "";
 
             try {
-
                 let listExt = "?listid=" + valueList.id;
-                //console.log(urlTask + listExt);
                 let respTask = await fetch(urlTask + listExt, cfg);
                 let dataTask = await respTask.json();
 
@@ -201,33 +152,22 @@ async function get_list() {
                     <li><a id="${deleteTaskIDinc}" class="delete">Delete</a></li>
                     </ul>
                     </label>
-
                     </div>`;
 
                     taskIdArray.push(valueTask.id);
-
                     idTaskInc++;
-
                 }
 
             } catch (error) {
                 console.log("Error with task");
             }
 
-            console.log("taskINC:" + idTaskInc);
-
             let div = document.createElement("div");
             div.id = valueList.id;
             div.className = "list";
             div.innerHTML = html + htmlTask;
-
-            
-
             div_list.appendChild(div);
-
-            
             confirmDelete.addEventListener("click", function(){
-                //console.log(logindata.userid);
                 delete_user(logindata.userid)
             });
 
@@ -243,20 +183,17 @@ async function get_list() {
 
             let deleteList = document.getElementById(deleteID);
             deleteList.addEventListener("click", function () {
-                //console.log(deleteList)
                 delete_list(valueList.id);
             })
 
 
             editName.addEventListener("click", function () {
-                console.log(editName);
                 editName.style.color = "#747474";
                 editName.style.background = "#ffffff";
             })
 
             let makeTask = document.getElementById(makeTaskID);
             makeTask.addEventListener("click", function () {
-                console.log(makeTask);
                 taskForm.style.display = "block";
                 makeTask.style.display = "none";
                 taskInputName.select();
@@ -265,23 +202,16 @@ async function get_list() {
 
             let taskForm = document.getElementById(formID);
             taskForm.addEventListener("click", function () {
-                console.log(taskForm);
             })
 
             let taskInputName = document.getElementById(taskInputID);
-
             let addTask = document.getElementById(addTaskID)
             addTask.addEventListener("click", function () {
-
-
-                //console.log("Added task" + taskInputName.value);
-
                 add_task(taskInputName.value, valueList.id);
             })
 
             let cancelButton = document.getElementById(cancelButtonID)
             cancelButton.addEventListener("click", function () {
-                console.log("Cancel task making");
                 taskForm.style.display = "none";
                 makeTask.style.display = "block";
                 taskInputName.value = "";
@@ -292,57 +222,41 @@ async function get_list() {
                 if (makePublic.innerHTML == textLabelMakePublic) {
                     switch_public(valueList.id, true);
                     makePublic.innerHTML = textLabelMakePrivate;
-
                     return;
                 } else {
                     switch_public(valueList.id, false);
                     makePublic.innerHTML = textLabelMakePublic;
-
                 }
             });
 
-            // Execute a function when the user releases a key on the keyboard
             taskInputName.addEventListener("keyup", function (event) {
-                // Number 13 is the "Enter" key on the keyboard
                 if (event.keyCode === 13) {
-                    // Cancel the default action, if needed
                     event.preventDefault();
-                    // Trigger the button element with a click
                     add_task(taskInputName.value, valueList.id);
                 }
                 if (event.keyCode === 27) {
-                    // Cancel the default action, if needed
                     event.preventDefault();
-                    // Trigger the button element with a click
                     taskForm.style.display = "none";
                     makeTask.style.display = "block";
                 }
             });
 
-            // Execute a function when the user releases a key on the keyboard
             editName.addEventListener("keyup", function (event) {
-                // Number 13 is the "Enter" key on the keyboard
                 if (event.keyCode === 13) {
-                    // Cancel the default action, if needed
                     event.preventDefault();
-                    // Trigger the button element with a click
                     editName.style.color = "#384e6e";
                     editName.style.background = "none";
                     editName.blur();
-
                     change_name_list(valueList.id, editName.value);
                 }
                 if (event.keyCode === 27) {
-                    // Cancel the default action, if needed
                     event.preventDefault();
-                    // Trigger the button element with a click
                     editName.style.color = "#384e6e";
                     editName.style.background = "none";
                     editName.blur();
                 }
             });
 
-            // A NEW TASK STUFF
             if (idTaskInc != 1) {
                 for (let i = 1; i < idTaskInc; i++) {
                     let taskButtonIDinc = taskButtonID+i;
@@ -353,12 +267,10 @@ async function get_list() {
 
                     let taskButton = document.getElementById(taskButtonIDinc)
                     taskButton.addEventListener("click", function () {
-                        console.log("task Dropdown List");
                     })
 
                     let taskName = document.getElementById(taskNameIDinc)
                     taskName.addEventListener("click", function () {
-                        console.log("Editing Tasks Name");
                         taskName.style.color = "#747474";
                         taskName.style.background = "#ffffff";
                         taskName.style.borderRadius = "5px";
@@ -373,44 +285,30 @@ async function get_list() {
                         }
                     })
 
-
                     let deleteTask = document.getElementById(deleteTaskIDinc);
                     deleteTask.addEventListener("click", function () {
-                        console.log("deleteTask");
                         delete_task(taskIdArray[i-1]);
                     })
 
-
-                    // Execute a function when the user releases a key on the keyboard
                     taskName.addEventListener("keyup", function (event) {
-                        // Number 13 is the "Enter" key on the keyboard
                         if (event.keyCode === 13) {
-                            // Cancel the default action, if needed
                             event.preventDefault();
-                            // Trigger the button element with a click
                             taskName.style.color = "white";
                             taskName.style.background = "#3f807a";
                             taskName.blur();
-
                             change_name_task(taskIdArray[i-1],taskName.value);
                         }
                         if (event.keyCode === 27) {
-                            // Cancel the default action, if needed
                             event.preventDefault();
-                            // Trigger the button element with a click
                             taskName.style.color = "white";
                             taskName.style.background = "#3f807a";
                             taskName.blur();
                         }
                     });
                 }
-            }
-
-            
+            }   
             idInc++;
-
         }
-
     }
     catch (err) {
         console.log(err);
@@ -418,16 +316,10 @@ async function get_list() {
 }
 
 async function add_list() {
-
-    //Get list url 
     let urlList = url + "/list"
-
-    //Get stored user data
     let logindata = JSON.parse(sessionStorage.getItem("user"));
-
     let token = logindata.token;
 
-    //Data to send 
     let updata = {
         name: addlist_name.value,
         userid: logindata.userid,
@@ -449,38 +341,25 @@ async function add_list() {
             throw data;
         }
         else {
-
             get_list();
-
         }
-
         addlist_name.value = "";
 
     }
     catch (err) {
-        // Find the error reason
         errorReason = "Name already in use or empty";
         addlist_txtResult.innerHTML = errorReason;
-
         console.log(err);
     }
 }
 
 async function delete_list(idValue) {
-
-    //Get list url 
     let urlList = url + "/list"
-
-    //Get stored user data
     let logindata = JSON.parse(sessionStorage.getItem("user"));
-
     let token = logindata.token;
-
-    //Data to send 
     let updata = {
         id: idValue,
     }
-
     let cfg = {
         method: "Delete",
         headers: {
@@ -497,31 +376,21 @@ async function delete_list(idValue) {
             throw data;
         }
         else {
-
             get_list();
-
         }
     }
     catch (err) {
-        // Find the error reason
         errorReason = "Name already in use or empty";
         addlist_txtResult.innerHTML = errorReason;
-
         console.log(err);
     }
 }
 
 async function add_task(nameValue, listidValue) {
-
-    //Get list url 
     let urlList = url + "/list/task"
-
-    //Get stored user data
     let logindata = JSON.parse(sessionStorage.getItem("user"));
-
     let token = logindata.token;
 
-    //Data to send 
     let updata = {
         name: nameValue,
         listid: listidValue,
@@ -543,30 +412,19 @@ async function add_task(nameValue, listidValue) {
             throw data;
         }
         else {
-            //get 
             get_list();
         }
     }
     catch (err) {
-        // Find the error reason
         errorReason = "Could not add task";
-
         console.log(errorReason);
     }
 }
 
 async function delete_task(idValue) {
-
-    //Get list url 
     let urltask = url + "/list/task";
-
-
-    //Get stored user data
     let logindata = JSON.parse(sessionStorage.getItem("user"));
-
     let token = logindata.token;
-
-    //Data to send 
     let updata = {
         id: idValue,
     }
@@ -587,27 +445,19 @@ async function delete_task(idValue) {
             throw data;
         }
         else {
-            //get 
             get_list();
         }
     }
     catch (err) {
-        // Find the error reason
         errorReason = "Could not delete task";
-
         console.log(errorReason);
     }
 }
 
-async function delete_user(idValue){
-        //Get list url 
+async function delete_user(idValue){ 
         let urlUser = url + "/user";
-
-
-        //Get stored user data
         let logindata = JSON.parse(sessionStorage.getItem("user"));
     
-        //Data to send 
         let updata = {
             id: idValue,
         }
@@ -628,26 +478,17 @@ async function delete_user(idValue){
                 throw data;
             }
             else {
-                
-                //Save user in cache
                 sessionStorage.removeItem("user");
-
-                //Move to page when logged in.
                 window.location.href = "index.html";
-                
             }
         }
         catch (err) {
-            // Find the error reason
             errorReason = "Could not delete task";
-    
             console.log(errorReason);
         }
 }
 
 async function switch_public(idValue, publicValue) {
-
-    //Get list url 
     let urlList = url + "/list/public";
 
     let updata = {
@@ -670,21 +511,14 @@ async function switch_public(idValue, publicValue) {
         if (resp.status > 202) {
             throw data;
         }
-
     }
     catch (err) {
-        // Find the error reason
         errorReason = "Could not change public state";
-
         console.log(err);
     }
-
-
 }
 
 async function change_name_list(idValue, nameValue) {
-
-    //Get list url 
     let urlList = url + "/list/name";
 
     let updata = {
@@ -707,23 +541,15 @@ async function change_name_list(idValue, nameValue) {
         if (resp.status > 202) {
             throw data;
         }
-
     }
     catch (err) {
-        // Find the error reason
         errorReason = "Could not change name";
-
         console.log(err);
     }
-
-
 }
 
 async function change_name_task(idValue, nameValue) {
-
-    //Get list url 
     let urlList = url + "/list/task/name";
-
     let updata = {
         name: nameValue,
         id: idValue,
@@ -744,16 +570,11 @@ async function change_name_task(idValue, nameValue) {
         if (resp.status > 202) {
             throw data;
         }
-
     }
     catch (err) {
-        // Find the error reason
         errorReason = "Could not change name";
-
         console.log(err);
     }
-
-
 }
 
 function show_form_add_list() {
@@ -771,9 +592,7 @@ function show_form_add_list() {
     addlist_name.select();
 }
 
-//Runs when delete user button is clicked
 function deleteUserWindow() {
-    console.log("Window Opened");
     if (deleteUserForm.style.maxHeight == "0px") {
         deleteUserForm.style.maxHeight = "300px";
     } else {
@@ -781,10 +600,8 @@ function deleteUserWindow() {
     }
 }
 
-//Runs when cancel button in delete user form is clicked
 function cancelResponse() {
     if (event.target == cancelDelete) {
-        console.log("Cancel");
         deleteUserForm.style.maxHeight = "0px";
     }
 }
--- public/js/login.js
+++ public/js/login.js
@@ -1,19 +1,15 @@
-//Base url 
+
 let url = "http://localhost:3000";
 
 window.addEventListener("load", setupLogin);
 
 function setupLogin() {
-    //### Variables 
-
-    //Login 
     let username_login = document.getElementById("username_login");
     let password_login = document.getElementById("password_login");
 
     let btnLogin = document.getElementById("btnLogin");
     let txtResult_login = document.getElementById("txtResult_login");
 
-    // Create user
     let email_register = document.getElementById("email_register");
     let username_register = document.getElementById("username_register");
     let password_register = document.getElementById("password_register");
@@ -22,28 +18,18 @@ function setupLogin() {
     let btnCreate = document.getElementById("btnCreate");
     let txtResult_register = document.getElementById("txtResult_register");
 
-    //### Eventlistners 
-    //Add register function to create button
     btnCreate.addEventListener('click', register_users);
-
-    //Add login function to login button
     btnLogin.addEventListener('click', login_user);
 
 }
 
-//Functions
-
-//Register user 
 async function register_users(evt) {
-        console.log("noe");
         let urlUsers = url + "/users"
-
         let updata = {
             email: email_register.value,
             password: password_register.value,
             username: username_register.value,
         }
-
         let cfg = {
             method: "POST",
             headers: { "Content-Type": "application/json" },
@@ -60,18 +46,15 @@ async function register_users(evt) {
             else {
                 throw data;
             }
-
         }
         catch (err) {
-            // Find the error reason
             errorReason = "Invalid input, check requirements.";
             txtResult_register.innerHTML = errorReason;
-
             console.log(err);
         }
 }
 
-//Login
+
 async function login_user(evt) {
     if (password_login.value == "" && username_login.value == "") {
         txtResult_login.innerHTML = "Please insert login information";
@@ -95,14 +78,8 @@ async function login_user(evt) {
         if (resp.status > 202) {
             throw (data);
         };
-        //console.log(data);
-        //Text to show you are logged in.
-        //txtResult_login.innerHTML = data.email + " is logged in!";
-
-        //Save user in cache
+        
         sessionStorage.setItem("user", JSON.stringify(data));
-
-        //Move to page when logged in.
         window.location.href = "board.html";
     }
     catch (err) {
